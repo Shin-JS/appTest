f709095231a86cb176f7c39b83213a73cf56e9af
JsShin <shin8702@gmail.com>
2021-06-09 21:05:27 +0900

    터처블 코어 컴포넌트

--- ch02_4/App.tsx
+++ ch02_4/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import {SafeAreaView, Alert, Button} from 'react-native'
+import {TouchableOpacity, TouchableHighlight, Text} from 'react-native'
+
+const onPress = () => Alert.alert('home pressed', 'message')
 
 export default function App(){
     return <SafeAreaView>
-        <Button title='Test'
-        onPress={()=> Alert.alert('testPress', 'hello world')}/>
+        <Button title='Test' onPress={onPress}/>
+        <TouchableOpacity onPress={onPress}>
+            <Text>TouchableOpacity</Text>
+        </TouchableOpacity>
+        <TouchableHighlight onPress={onPress}>
+            <Text>TouchableHighlight</Text>
+        </TouchableHighlight>
     </SafeAreaView>
 }

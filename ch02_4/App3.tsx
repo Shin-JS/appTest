7cbff69019b9909ae03e6ba8aa851d360176c062
JsShin <shin8702@gmail.com>
2021-06-09 21:13:08 +0900

    기본다지기 챕터 끝

--- ch02_4/App3.tsx
+++ ch02_4/App3.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {SafeAreaView, Alert, Button} from 'react-native'
 import {TouchableOpacity, TouchableHighlight, Text} from 'react-native'
-import {TextInput} from 'react-native'
 
 const onPress = () => Alert.alert('home pressed', 'message')
 
@@ -14,11 +13,5 @@ export default function App(){
         <TouchableHighlight onPress={onPress}>
             <Text>TouchableHighlight</Text>
         </TouchableHighlight>
-        <TextInput
-            placeholder="enter your name"
-            onChangeText={(text: string) => console.log(text)}
-            onFocus={() => console.log('onFocus')}
-            onBlur={() => console.log('onBlur')}
-            onEndEditing={()=>console.log('onEndEditing')}/>
     </SafeAreaView>
 }

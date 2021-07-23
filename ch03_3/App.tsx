53f9e3ad677e73bceac38fe337ab6a98b556b82c
JsShin <shin8702@gmail.com>
2021-07-23 14:51:21 +0900

    폰트 적용

--- ch03_3/App.tsx
+++ ch03_3/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react'
-//prettier-ignore
-import {StyleSheet, SafeAreaView, ImageBackground, Image} from 'react-native'
+// prettier-ignore
+import {StyleSheet, Platform, SafeAreaView, ImageBackground, Image, View, Text} from 'react-native'
 import * as D from './src/data'
 
 const avatarUrl = D.randomAvatarUrl()
 const avatarSize = 50
+
+const text = `Almost before we knew it, we had left the ground.`
+
 export default function App() {
   return (
-    <SafeAreaView style={[styles.flex]}>
+    <SafeAreaView style={styles.flex}>
       <ImageBackground
-        style={[styles.flex, styles.imageBackground]}
-        source={require('./src/assets/images/bg.jpg')}
-        >
-          <Image source={{uri: avatarUrl}} style={[styles.image]}/>
-        </ImageBackground>
+          style={styles.backgroundImage}
+          source={require('./src/assets/images/bg.jpg')}>
+          <Image source={{uri: avatarUrl}} style={[styles.image]} />
+          <View style={[styles.flex, styles.padding10]}>
+            <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
+            <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
+            <Text style={[styles.text, styles.semiBold]}>{text} [semi bold]</Text>
+            <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
+          </View>
+          </ImageBackground>
     </SafeAreaView>
   )
 }
 
+//prettier-ignore
 const styles = StyleSheet.create({
   flex: {flex: 1},
-  imageBackground: {padding: 10},
-  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2}
+  backgroundImage: {flex: 1},
+  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
+  padding10: {padding: 10},
+  text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10},
+
+  regular: {fontFamily: 'DancingScript-Regular', fontWeight: '400'},
+  medium: {fontFamily: 'DancingScript-Medium', fontWeight: '500'},
+  semiBold: {fontFamily: 'DancingScript-SemiBold', fontWeight: '600'},
+  bold: {
+    fontFamily: 'DancingScript-Bold',
+    fontWeight: Platform.select({ios: '700', android: '600'})
+  }
 })
